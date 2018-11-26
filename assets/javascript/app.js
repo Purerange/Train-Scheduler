ee3f9bc89a9025408dd1f5add29286d0757abd32
Stephen Marsh <38229184+Purerange@users.noreply.github.com>
2018-11-26 13:34:18 -0800

    final additions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,53 +18,94 @@ $("#submitButton").on("click", function(event) {
     var name = $("#nameInput").val().trim();
     var destination = $("#destinationInput").val().trim();
     var first = $("#firstInput").val().trim();
-    var frequency = $("#frequencyInput").val().trim();
     var firstHour = first.substring(0,2);
-    var firstSeconds = first.substring(3,5);
+    var firstMinutes = first.substring(3,5);
+    var frequency = $("#frequencyInput").val().trim();
+
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+
 
     // console.log("Name: " + name);
     // console.log("Destination: " + destination);
     // console.log("First train: " + first);
     // console.log("First train hour: " + firstHour);
-    // console.log("First train seconds: " + firstSeconds);
+    // console.log("First train minutes: " + firstMinutes);
     // console.log("Frequency: " + frequency);
+    // console.log("Current Time: " + today);
+    // console.log("Current Hours: " + h);
+    // console.log("Current Minutes: " + m);
 
 
-    // database.ref().push({
-    //     name: name,
-    //     first: first,
-    //     frequency: frequencyValue,
-    //     months: months,
-    //     destination: destination,
-    //     earnings: earnings
-    // });
+    database.ref().push({
+        name: name,
+        destination: destination,
+        first: first,
+        firstHour: firstHour,
+        firstMinutes: firstMinutes,
+        frequency: frequency
+    });
 
-    // var tr = $("<tr>");
-    // var tdName = $("<td>");
-    // var tdfirst = $("<td>");
-    // var tdfrequency = $("<td>");
-    // var tdMonths = $("<td>");
-    // var tddestination = $("<td>");
-    // var tdEarnings = $("<td>");
+});
 
-    // tdName.text(name);
-    // tdfirst.text(first);
-    // tdfrequency.text(frequencyValue);
-    // tdMonths.text(months);
-    // tddestination.text(destination);
-    // tdEarnings.text(earnings);
+database.ref().on("value", function(snapshot) {
 
-    // tr.append(tdName);
-    // tr.append(tdfirst);
-    // tr.append(tdfrequency);
-    // tr.append(tdMonths);
-    // tr.append(tddestination);
-    // tr.append(tdEarnings);
+    $("#table").empty();
 
-    // $(".table").append(tr);
+    // console.log(snapshot.val());
 
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
 
+    snapshot.forEach(function(childNodes) {
+        var current = childNodes.val();
+        var name = current.name;
+        var destination = current.destination;
+        var first = current.first;
+        var firstHour = current.firstHour;
+        var firstMinutes = current.firstMinutes;
+        var frequency = current.frequency;
 
+        var tr = $("<tr>");
+        var tdName = $("<td>");
+        var tdDestination = $("<td>");
+        var tdFrequency = $("<td>");
+        var tdNext = $("<td>");
+        var tdMinutesAway = $("<td>");
     
-
-})
+        tdName.text(name);
+        tdDestination.text(destination);
+        tdFrequency.text(frequency);
+    
+        var next = "";
+        var minutesAway;
+    
+        if ((parseFloat(firstHour) * 60 + parseFloat(firstMinutes)) > (parseFloat(h) * 60 + parseFloat(m))) {
+            next = first;
+            minutesAway = (parseFloat(firstHour) * 60 + parseFloat(firstMinutes) - (parseFloat(h) * 60 + parseFloat(m)));
+        } else {
+            minutesAway = (frequency - (m % frequency));
+            if (m + minutesAway >= 60) {
+                next = "" + (h+1) + ":" + (m + minutesAway - 60);
+            } else {
+                next = "" + h + ":" + (m + minutesAway);
+            }
+        }
+    
+        // console.log("Next train: " + next);
+        // console.log("Minutes Away: " + minutesAway);
+    
+        tdNext.text(next);
+        tdMinutesAway.text(minutesAway);
+    
+        tr.append(tdName);
+        tr.append(tdDestination);
+        tr.append(tdFrequency);
+        tr.append(tdNext);
+        tr.append(tdMinutesAway);
+    
+        $("#table").append(tr);
+    })
+});
